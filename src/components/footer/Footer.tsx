e3b6cf17fb725a79e2970a81ebaddd17c73467e8
zharsuke <alazharisqi@gmail.com>
2024-11-09 21:26:37 +0700

    fix: change static year to dynamic year on footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
+    
     return (
         <footer className="footer footer-center p-4 border-dashed border-t-2 border-primary text-base-content mt-10">
             <aside>
-                <p>Copyright © 2024 - All right reserved by zharsuke</p>
+                <p>Copyright © {currentYear} - All right reserved by zharsuke</p>
             </aside>
         </footer>
     )
