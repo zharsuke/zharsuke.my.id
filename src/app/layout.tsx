8413db273a31dfb629d33ba6d6b45500e3425441
zharsuke <alazharisqi@gmail.com>
2024-11-10 17:47:28 +0700

    feat: switch theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="valentine">
+    <html lang="en" data-theme="synthwave">
       <body className={font.className}>
         {children}
       </body>
