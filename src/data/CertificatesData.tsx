53c3c10c9f1f702560e100db25cbb866dfa23f66
zharsuke <alazharisqi@gmail.com>
2024-11-11 10:59:31 +0700

    feat: add course cert

--- src/data/CertificatesData.tsx
+++ src/data/CertificatesData.tsx
@@ -13,19 +13,19 @@ const certificatesData = [
     },
     {
         id: 3,
-        title: "Intercomp 2023 Cyber Security - 2nd Place",
+        title: "Intercomp'23 Cyber Security - 2nd Place",
         image: "/certificates/ctf/intercomp2023-2nd.png",
         tag: "Competition",
     },
     {
         id: 4,
-        title: "AceCTF 2024 Cyber Security - Finalist",
+        title: "AceCTF'24 Cyber Security - Finalist",
         image: "/certificates/ctf/acectf2024-finalis.jpg",
         tag: "Competition",
     },
     {
         id: 5,
-        title: "HackToday 2024 Cyber Security - Finalist",
+        title: "HackToday'24 Cyber Security - Finalist",
         image: "/certificates/ctf/hacktoday2024-finalis.jpg",
         tag: "Competition",
     },
@@ -43,10 +43,82 @@ const certificatesData = [
     },
     {
         id: 8,
-        title: "VishwaCTF Cyber Security - Participant",
-        image: "/certificates/ctf/vishwactf-peserta.jpg",
-        tag: "Competition",
-    }
+        title: "Cisco - Ethical Hacker",
+        image: "/certificates/cisco/Ethical_Hacker.jpg",
+        tag: "Course",
+    },
+    {
+        id: 9,
+        title: "Cisco - Cyber Security Essentials",
+        image: "/certificates/cisco/Junior_Cybersecurity_Analyst_Career_Path.jpg",
+        tag: "Course",
+    },
+    {
+        id: 10,
+        title: "Cisco - Network Defense",
+        image: "/certificates/cisco/Network_Defense.jpg",
+        tag: "Course",
+    },
+    {
+        id: 11,
+        title: "Cisco - Networking Devices and Initial Config",
+        image: "/certificates/cisco/Networking_Devices_and_Initial_Configuration.jpg",
+        tag: "Course",
+    },
+    {
+        id: 12,
+        title: "Cisco - Networking Basics",
+        image: "/certificates/cisco/Networking_Basics.jpg",
+        tag: "Course",
+    },
+    {
+        id: 13,
+        title: "Cisco - Introduction to Cybersecurity",
+        image: "/certificates/cisco/Introduction_to_Cybersecurity.jpg",
+        tag: "Course",
+    },
+    {
+        id: 14,
+        title: "Dicoding - Back-End Applications for Beginners",
+        image: "/certificates/dicoding/Belajar Membuat Aplikasi Back-End untuk Pemula.jpg",
+        tag: "Course",
+    },
+    {
+        id: 15,
+        title: "Dicoding - Front-End Web for Beginners",
+        image: "/certificates/dicoding/Belajar Membuat Front-End Web untuk Pemula.jpg",
+        tag: "Course",
+    },
+    {
+        id: 16,
+        title: "Dicoding - Basic JavaScript Programming",
+        image: "/certificates/dicoding/Belajar Dasar Pemrograman JavaScript.jpg",
+        tag: "Course",
+    },
+    {
+        id: 17,
+        title: "Dicoding - Basic Web Programming",
+        image: "/certificates/dicoding/Belajar Dasar Pemrograman Web.jpg",
+        tag: "Course",
+    },
+    {
+        id: 18,
+        title: "Dicoding - Cloud Practitioner Essentials (AWS)",
+        image: "/certificates/dicoding/Cloud Practitioner Essentials (Belajar Dasar AWS Cloud).jpg",
+        tag: "Course",
+    },
+    {
+        id: 19,
+        title: "Dicoding - Basic SQL",
+        image: "/certificates/dicoding/Belajar Dasar Structured Query Language (SQL).jpg",
+        tag: "Course",
+    },
+    {
+        id: 20,
+        title: "Dicoding - Basic Java",
+        image: "/certificates/dicoding/Memulai Pemrograman Dengan Java.jpg",
+        tag: "Course",
+    },
 ]
 
 export default certificatesData;
