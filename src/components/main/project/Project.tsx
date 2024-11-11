b66b39fb4ad8c6172ba947bf33c3ee42c224bd27
zharsuke <alazharisqi@gmail.com>
2024-11-11 11:45:51 +0700

    feat: adjust project layout

--- src/components/main/project/Project.tsx
+++ src/components/main/project/Project.tsx
@@ -4,17 +4,25 @@ import Link from "next/link";
 export default function Project() {
     const projects = projectData.toReversed();
     return (
-        <div className="grid gap-4 md:grid-cols-3">
+        <div className="grid gap-4 md:grid-cols-3 mb-4">
             {projects.map((project) => (
                 <div key={project.id} className="p-2 border-dashed border border-primary rounded-md">
-                    <Link key={project.id} href={`/projects/${project.id}`}>
-
-                        <h1 className="font-bold text-sm md:text-lg">{project.title}</h1>
-                        <p className="text-sm md:text-lg my-2">{project.subtitle}</p>
-                        <div className="flex flex-row gap-2">
-                            {project.tech && project.tech.map((tech) => (
-                                <p className="text-xs md:text-xs p-1 border-dashed border border-primary rounded" key={tech}>{tech}</p>
-                            ))}
+                    <Link href={`/projects/${project.id}`}>
+                        <div className="flex flex-col h-full justify-between rounded-md">
+                            <div>
+                                <h1 className="font-bold text-sm md:text-lg">{project.title}</h1>
+                                <p className="text-sm md:text-lg my-2">{project.subtitle}</p>
+                                <div className="flex flex-row gap-2">
+                                    <p className="font-bold hover:text-primary transition ease-in-out delay-150 hover:-translate-y-0.5 duration-300">Read More...</p>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap gap-2 mt-4">
+                                {project.tech && project.tech.map((tech) => (
+                                    <p className="text-xs md:text-xs p-1 border-dashed border border-primary rounded" key={tech}>
+                                        {tech}
+                                    </p>
+                                ))}
+                            </div>
                         </div>
                     </Link>
                 </div>
