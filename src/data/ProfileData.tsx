fd170dd5b586b5d4df10ca657e15fc9ad7d89085
zharsuke <alazharisqi@gmail.com>
2024-11-13 02:04:23 +0700

    feat: minor adjust

--- src/data/ProfileData.tsx
+++ src/data/ProfileData.tsx
@@ -1,7 +1,7 @@
 const profileData = {
     "image": "https://avatars.githubusercontent.com/u/68967912?v=4",
     "name": "AL AZHAR RIZQI RIFA'I FIRDAUS",
-    "description": "Call me Azhar, web universe enthusiast. Student at State Polytechnic of Malang.",
+    "description": "Web universe enthusiast. Student at State Polytechnic of Malang.",
     "email": "mailto:alazharisqi@gmail.com",
     "github": "https://github.com/zharsuke",
     "linkedin": "https://www.linkedin.com/in/al-azhar-rizqi-rifa-i-firdaus-384236221/",
