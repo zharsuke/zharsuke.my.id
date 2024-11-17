7cc4a1b8b2e4996fe80ecc1009eebe699d05cff5
zharsuke <alazharisqi@gmail.com>
2024-11-18 02:21:43 +0700

    feat: add post

--- src/data/PostData.tsx
+++ src/data/PostData.tsx
@@ -1,15 +1,45 @@
 const postData = [
     {
-        id: 3,
-        title: "Read “Hack the Box: Drive Walkthrough“ by Al Azhar Rizqi on Medium",
-        image: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*SPXiJ6EzuZ1wzMhwUKp6jw.jpeg",
-        link: "https://medium.com/@zharsuke/hack-the-box-drive-walkthrough-bd22f1320755"
+        id: 7,
+        title: "Read “Mahasiswa Jurusan Teknologi Informasi (JTI) Polinema kembali meraih prestasi pada ajang GEMASTIK...“ by polinema_campus on Instagram",
+        image: "/image-post/polinema-gemastik.jpg",
+        link: "https://www.instagram.com/p/DAsEpDAyQPa/?igsh=MXM3bWczaTRiY2l5bg=="
+    },
+    {
+        id: 10,
+        title: "Read “Selamat kepada para pemenang serta finalis dari kontingen Polinema yang telah mengikuti KMIPN V 2023...“ by wri_polinema on Instagram",
+        image: "/image-post/wri-kmipnv.jpg",
+        link: "https://www.instagram.com/p/Cvl6v9WrBsm/?img_index=7&igsh=MWU4b2t5bHQ4YXN6YQ== ",
+    },
+    {
+        id: 8,
+        title: "Read “kepikiran 5W1H-nya CTF apa ya kita-kira... nah kalo kalian penasaran👀 langsung yuk join BRAIN BOOST dan catat tanggal nya!“ by wri_polinema on Instagram",
+        image: "/image-post/wri-brainboost.jpg",
+        link: "https://www.instagram.com/p/C54-GoZLG6F/?igsh=MTJ0NjJ1aWRvMnJjZQ=="
+    },
+    {
+        id: 11,
+        title: "Read “[PEMENANG INTERNAL COMPETITION JURUSAN TEKNOLOGI INFORMASI 2023]...“ by wri_polinema on Instagram",
+        image: "/image-post/intercomp23.jpg",
+        link: "https://www.instagram.com/p/CsBDjmCrMLb/?img_index=2&igsh=Mm54N3V5Njh5dDQ5"
+    },
+    {
+        id: 9,
+        title: "Documentation of Cybersecurity Extracurricular Activities at SMKN 4 Malang During Mentorship Sessions",
+        image: "/image-post/ekskul-cyber.jpg",
+        link: "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTA2MzE1NjgzNzIwMjg0?story_media_id=3487924537160136102_3019452340&igsh=MTRrajc4YTM1ejZybg%3D%3D",
     },
     {
         id: 6,
         title: "Watch “Source Code Disclosure - Git“ by zharsuke on TikTok",
         image: "/image-post/Source Code Disclosure-Cover.jpg",
         link: "https://vt.tiktok.com/ZSjDbCWLu/",
+    },
+    {
+        id: 3,
+        title: "Read “Hack the Box: Drive Walkthrough“ by Al Azhar Rizqi on Medium",
+        image: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*SPXiJ6EzuZ1wzMhwUKp6jw.jpeg",
+        link: "https://medium.com/@zharsuke/hack-the-box-drive-walkthrough-bd22f1320755"
     },
     {
         id: 1,
