878653a0b24be54083dd421107092922ffdb65b2
zharsuke <alazharisqi@gmail.com>
2024-11-19 10:18:13 +0700

    fix: minor adjust add aria

--- src/components/main/profile/ProfileBody.tsx
+++ src/components/main/profile/ProfileBody.tsx
@@ -19,12 +19,12 @@ export default function ProfileBody({ name, description, email, github, linkedin
             <h1 className="text-base md:text-base font-bold mb-2">{name}</h1>
             <p className="text-sm sm:text-sm md:text-base mb-3">{description}</p>
             <div className="flex gap-2 mt-3 text-base">
-                <Link href={email} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`}><Icon icon="line-md:email" aria-label="Send email"/></Link>
-                <Link href={github} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`}><Icon icon="line-md:github-loop" aria-label="View github" /></Link>
-                <Link href={linkedin} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`}><Icon icon="line-md:linkedin" aria-label="View linkedin" /></Link>
-                <Link href={twitter} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`}><Icon icon="line-md:twitter" aria-label="View twitter" /></Link>
-                <Link href={medium} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`}><Icon icon="ri:medium-line" aria-label="View medium" /></Link>
-                <a href={resume} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`}><Icon icon="line-md:file-document" aria-label="View resume" /></a>
+                <Link href={email} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`} aria-label="Send email"><Icon icon="line-md:email"/></Link>
+                <Link href={github} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`} aria-label="View github"><Icon icon="line-md:github-loop" /></Link>
+                <Link href={linkedin} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`} aria-label="View linkedin"><Icon icon="line-md:linkedin" /></Link>
+                <Link href={twitter} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`} aria-label="View twitter"><Icon icon="line-md:twitter" /></Link>
+                <Link href={medium} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`} aria-label="View medium"><Icon icon="ri:medium-line" /></Link>
+                <a href={resume} className={`border-solid border border-primary px-1 py-1 rounded-md ${hover}`} aria-label="View resume"><Icon icon="line-md:file-document" /></a>
             </div>
         </div>
     )
