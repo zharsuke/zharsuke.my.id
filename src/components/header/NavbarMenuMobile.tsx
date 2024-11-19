54f5df6969460767497414bc32501f56f6bd49d8
zharsuke <alazharisqi@gmail.com>
2024-11-19 10:04:03 +0700

    fix: minor adjust add aria

--- src/components/header/NavbarMenuMobile.tsx
+++ src/components/header/NavbarMenuMobile.tsx
@@ -8,7 +8,7 @@ export default function NavbarMenu({ hover }: NavbarMenuProps) {
     return (
         <div className="sm:hidden">
             <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" aria-label="Menu Toggle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                 </div>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-10 z-[1] right-0 top-0 p-3 shadow bg-base-100 rounded-box w-52">
