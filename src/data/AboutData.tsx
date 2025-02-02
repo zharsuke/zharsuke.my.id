85d3dcadd1ea315b36894311b54b66cc41d36ee7
zharsuke <alazharisqi@gmail.com>
2025-02-02 11:26:14 +0700

    feat: update about

--- src/data/AboutData.tsx
+++ src/data/AboutData.tsx
@@ -1,5 +1,5 @@
 const aboutData = {
-    "about": "A student at the State Polytechnic of Malang with deeply engaged in cybersecurity, actively practicing on Hack The Box, mentoring others, and participating in competitions. I also share insights through Medium write-ups. With around four years of programming experience, I specialize in web development using frameworks like Laravel, Django, and NextJS.",
+    "about": "A student at the State Polytechnic of Malang with deeply engaged in cybersecurity, actively practicing on Hack The Box, mentoring others, and participating in competitions. I also share insights through Medium write-ups. With around four years of programming experience, I specialize in web development using frameworks like Laravel, Django, NextJS, ExpressJS, and AngularJS.",
 }
 
 export default aboutData;
